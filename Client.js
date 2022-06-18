47c0ecca45363a368f1e4fdb2fa9616fbc4818ad
Nate Notermann <nate.notermann@gmail.com>
2022-06-18 16:02:45 -0500

    added object to hold employee info

--- Client.js
+++ Client.js
@@ -2,11 +2,27 @@ $(document).ready(onReady); // first on ready code
 console.log( 'js' ); // confirm javascript is connected
 
 function onReady (){ 
-
-    $( '.submitButton' ).on( 'click' , addEmployee ); // click-listener for submit button
+    // click-listener for submit button
+    $( '.submitButton' ).on( 'click' , addEmployee ); 
 }
 
-function addEmployee () {
-    console.log( 'test submit button function' ); //test submit button function -works
+function addEmployee () { // this will be the main function that does most of the work
+    // console.log( 'test submit button function' ); //test submit button function -works
+    //creates a variable to save employee info
+    let employeeObject = { 
+        firstName: 'testfirst',
+        lastName: 'testlast',
+        id: 'testID',
+        title: 'testtitle',
+        annualSalary: 'testsalary',
+    }
+console.log( employeeObject, employeeObject.firstName ); //test object - works
 
-}
+
+
+
+
+
+} // ---- END of main "addEmployee" function ----
+
+
